c91b79cf92f273198ed15ce6602b7f0fd68871c8
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-05-14 20:41:00 +0600

    module-5 done

---
+++ module-5/video-2-.ts
@@ -0,0 +1,15 @@
+// import * allFn from './'
+// allFn.add => to run the function
+// You can create a new file which includes all imports file and exports as named export
+
+/**
+ * import f1 from './'
+ * import f2 from './'
+ * import f3 from './'
+ * import f4 from './'
+ * import f5 from './'
+ *
+ * export default {f1, f2, f3, f4, f5}
+ *
+ * import {f1, f2, f3, f4, f5} from './'
+ */
---
+++ module-5/video-3-.ts
@@ -0,0 +1,69 @@
+/**
+ *! Pick, Omit
+ *! Partial
+ *! Required
+ *! ReadOnly
+ *! Record
+ *  */
+
+/@ Pick */;
+type Person = {
+  name: string;
+  email: string;
+  age: number;
+};
+
+type EmailT = Pick<Person, "email">;
+type BothT = Pick<Person, "name" | "age">;
+
+/@  Partial */;
+// to make all property as optional
+type Property = {
+  a: string;
+  b: string;
+  c: string;
+};
+
+type OProperty = Partial<Property>;
+// type OProperty = {
+//   a?: string;
+//   b?: string;
+//   c?: string;
+// };
+
+/@ Required  */;
+type PropertyR = {
+  a?: string;
+  b: string;
+  c?: string;
+};
+type RProperty = Required<PropertyR>;
+
+// type RProperty = {
+//   a: string;
+//   b: string;
+//   c: string;
+// };
+
+/@ ReadOnly  */;
+type PropertyReadOnly = {
+  a: string;
+  b: string;
+  c: string;
+};
+type ReadProperty = Readonly<PropertyReadOnly>;
+// type ReadProperty = {
+//   readonly a: string;
+//   readonly b: string;
+//   readonly c: string;
+// };
+
+/@ Record */;
+
+type myObj = Record<"a" | "b" | "c" | "d", string>;
+// type myObj = {
+//   a: string;
+//   b: string;
+//   c: string;
+//   d: string;
+// };
